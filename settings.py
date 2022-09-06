aabe04242073dfc210f441cf922be53bf851c3de
paulego <egopaul@inbox.ru>
2022-09-06 10:50:00 +0500

    Added token verification in the environment

--- settings.py
+++ settings.py
@@ -1,7 +1,12 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-BITLY_BEARER_TOKEN = os.environ.get('BITLY_BEARER_TOKEN')
+try:
+    BITLY_BEARER_TOKEN = os.environ['BITLY_BEARER_TOKEN']
+except KeyError as error:
+    print('Missing authorization token')
+    sys.exit()
